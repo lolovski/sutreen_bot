192c47ab706cede1d0d4716ef29e95465a21451b
lolovski <alexpaxyaev@gmail.com>
2025-04-04 21:59:45 +0300

    main

--- main.py
+++ main.py
@@ -23,5 +23,5 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    #logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
